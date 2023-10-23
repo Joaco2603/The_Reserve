6ba7e4d826b5ae3546f2a0f5ebcbef2d05df0dcf
Joaco2603 <111933983+Joaco2603@users.noreply.github.com>
2023-10-23 17:37:49 -0600

    Juego terminado con algunos bugs

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -5,18 +5,25 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using System;
+using System.Net;
+using System.Net.Sockets;
 
 
-public class UIManager : Singleton<UIManager>
+public class UIManager : NetworkSingleton<UIManager>
 {
     // [SerializeField]
     // private Button startServerButton;
 
-    // [SerializeField]
-    // private Button startHostButton;
+    [SerializeField]
+    private Button startHostButton;
 
-    // [SerializeField]
-    // private Button startClientButton;
+    [SerializeField]
+    private Button startClientButton;
+
+    [SerializeField]
+    private Button Instructions;
 
     [SerializeField]
     private Image MainImage;
@@ -45,7 +52,17 @@ public class UIManager : Singleton<UIManager>
     [SerializeField]
     private Image restarMenu;
 
+    [SerializeField]
+    private GameObject IntructionsMenu;
+
+    [SerializeField]
+    private Button exitInstructions;
+
     private string timeliType;
+
+    // Puerto que quieres verificar
+    private const int port = 7777;
+
 
 
     // [SerializeField]
@@ -65,29 +82,54 @@ public class UIManager : Singleton<UIManager>
     void Start()
     {
 
-        startGame?.onClick.AddListener(async () =>
-        {
-            if (!NetworkManager.Singleton.IsServer && NetworkManager.Singleton.StartHost())
-            {
-                // No hay servidor, iniciar como host
-                if (RelayManager.Instance.IsRelayEnabled) 
-                await RelayManager.Instance.SetupRelay();
-                    Logger.Instance.LogInfo("Host started...");
-                NetworkManager.Singleton.StartHost();
-                EnabledMainMenu();
-            }
-            else
-            {
-                if (RelayManager.Instance.IsRelayEnabled && !string.IsNullOrEmpty(joinCodeInput.text))
-                    await RelayManager.Instance.JoinRelay(joinCodeInput.text);
+        
+    // startGame.onClick.AddListener(async () => 
+    // {
+    //     EnabledMainMenu();
 
-                    if(NetworkManager.Singleton.StartClient())
-                        Logger.Instance.LogInfo("Client started...");
-                    else
-                        Logger.Instance.LogInfo("Unable to start client...");
-                EnabledMainMenu();
-            } 
-        });
+    //     IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+    //     IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, port);
+
+
+    //     using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+    //     {
+    //         try
+    //         {
+    //             socket.Connect(ipEndPoint);
+    //             Debug.Log("Puerto " + port + " está en uso.");
+    //             bool clientStarted = NetworkManager.Singleton.StartClient();
+    //             if (clientStarted)
+    //             {
+    //                 Logger.Instance.LogInfo("Client started...");
+    //             }
+    //             else
+    //             {
+    //                 Logger.Instance.LogInfo("Unable to start client...");
+    //             }
+    //         }
+    //         catch (SocketException)
+    //         {
+    //             Debug.Log("Puerto " + port + " está disponible.");
+    //             if (RelayManager.Instance.IsRelayEnabled) 
+    //             {
+    //                 await RelayManager.Instance.SetupRelay();
+    //             }
+        
+    //             // Intenta iniciar el host
+    //             bool hostStarted = NetworkManager.Singleton.StartHost();
+    //             if (!hostStarted)
+    //             {
+    //                 throw new Exception("Unable to start host");
+    //             }
+        
+    //             Logger.Instance.LogInfo("Host started...");
+    //         }
+    //     }
+    // });
+
+
+        
+        
 
 
 
@@ -102,31 +144,49 @@ public class UIManager : Singleton<UIManager>
         // });
 
         // // START HOST
-        // startHostButton?.onClick.AddListener(async () =>
-        // {
-        //     // this allows the UnityMultiplayer and UnityMultiplayerRelay scene to work with and without
-        //     // relay features - if the Unity transport is found and is relay protocol then we redirect all the 
-        //     // traffic through the relay, else it just uses a LAN type (UNET) communication.
-        //     if (RelayManager.Instance.IsRelayEnabled) 
-        //         await RelayManager.Instance.SetupRelay();
+        startHostButton?.onClick.AddListener(async () =>
+        {
+            EnabledMainMenu();
+            // this allows the UnityMultiplayer and UnityMultiplayerRelay scene to work with and without
+            // relay features - if the Unity transport is found and is relay protocol then we redirect all the 
+            // traffic through the relay, else it just uses a LAN type (UNET) communication.
+            if (RelayManager.Instance.IsRelayEnabled) 
+                await RelayManager.Instance.SetupRelay();
 
-        //     if (NetworkManager.Singleton.StartHost())
-        //         Logger.Instance.LogInfo("Host started...");
-        //     else
-        //         Logger.Instance.LogInfo("Unable to start host...");
-        // });
+            if (NetworkManager.Singleton.StartHost())
+                Logger.Instance.LogInfo("Host started...");
+            else
+                Logger.Instance.LogInfo("Unable to start host...");
+        });
 
         // // START CLIENT
-        // startClientButton?.onClick.AddListener(async () =>
-        // {
-        //     if (RelayManager.Instance.IsRelayEnabled && !string.IsNullOrEmpty(joinCodeInput.text))
-        //         await RelayManager.Instance.JoinRelay(joinCodeInput.text);
+        startClientButton?.onClick.AddListener(async () =>
+        {
+            EnabledMainMenu();
+            if (RelayManager.Instance.IsRelayEnabled && !string.IsNullOrEmpty(joinCodeInput.text))
+                await RelayManager.Instance.JoinRelay(joinCodeInput.text);
 
-        //     if(NetworkManager.Singleton.StartClient())
-        //         Logger.Instance.LogInfo("Client started...");
-        //     else
-        //         Logger.Instance.LogInfo("Unable to start client...");
-        // });
+            if(NetworkManager.Singleton.StartClient())
+                Logger.Instance.LogInfo("Client started...");
+            else
+                Logger.Instance.LogInfo("Unable to start client...");
+        });
+
+
+        Instructions?.onClick.AddListener(async () =>
+        {
+            EnabledButtons(false);
+            exitInstructions.gameObject.SetActive(true);
+            IntructionsMenu.SetActive(true);
+        });
+
+        exitInstructions?.onClick.AddListener(async () =>
+        {
+            EnabledButtons(true);
+            IntructionsMenu.SetActive(false);
+            exitInstructions.gameObject.SetActive(false);
+        });
+
 
         // STATUS TYPE CALLBACKS
         NetworkManager.Singleton.OnClientConnectedCallback += (id) =>
@@ -134,6 +194,15 @@ public class UIManager : Singleton<UIManager>
             Logger.Instance.LogInfo($"{id} just connected...");
         };
     }
+
+    private void EnabledButtons(bool type)
+    {
+        foreach(var item in buttonHiden)
+        {
+            item.gameObject.SetActive(type);
+        }
+    }
+
 
     private void EnabledMainMenu()
     {
